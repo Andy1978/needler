ee1a03b980100f5c12575f908e2bff911bcdb563
Andreas Weber <andy.weber.aw@gmail.com>
2013-05-23 17:52:15 +0200

    added COPYING GPLv3, update of README and the docs

---
+++ doc/wget_docs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -nc www.atmel.com/Images/doc2503.pdf
+wget -nc http://download.savannah.gnu.org/releases/avr-libc/avr-libc-user-manual-1.8.0.pdf.bz2
+wget -nc http://www.ftdichip.com/Support/Documents/DataSheets/Cables/DS_USB_RS232_CABLES.pdf
+wget -nc http://www.cnc-plus.de/PDF/AMW%20102-V3.pdf
