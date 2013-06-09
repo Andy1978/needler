1d973e1c665c044ff55302c2ba10d4f4b0e3deb2
Andreas Weber <andy.weber.aw@gmail.com>
2013-06-09 11:48:08 +0200

    ACR Appnote 8x8 Matrix hinzugefuegt

--- doc/wget_docs.sh
+++ doc/wget_docs.sh
@@ -3,3 +3,4 @@ wget -nc www.atmel.com/Images/doc2503.pdf
 wget -nc http://download.savannah.gnu.org/releases/avr-libc/avr-libc-user-manual-1.8.0.pdf.bz2
 wget -nc http://www.ftdichip.com/Support/Documents/DataSheets/Cables/DS_USB_RS232_CABLES.pdf
 wget -nc http://www.cnc-plus.de/PDF/AMW%20102-V3.pdf
+wget -nc http://www.atmel.com/Images/doc2532.pdf
